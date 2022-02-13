0b6f80251cf674ae202149521887aecf24ce6248
Nikolay Pushkarev <NeverEverLives@gmail.com>
2022-02-13 22:47:06 +0300

    Add camera and overlap
    Add custom draw method
    Function:
    - camera
    Take player position (bind camera to player) and add player offset for move camera
    - overlap
    Now work with hitbox. Not with rectangle. And on draw stage sort tile and player.
    First draw tile and then player.

--- code/level.py
+++ code/level.py
@@ -5,17 +5,18 @@ from tile import Tile
 from player import Player
 from debug import debug
 
+
 class Level:
     def __init__(self) -> None:
 
-        self.diplay_surface = pygame.display.get_surface()
+        self.display_surface = pygame.display.get_surface()
 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
 
-    def create_map(self):
+    def create_map(self) -> None:
         for row_index, row in enumerate(WORLD_MAP):
             for colimn_index, column in enumerate(row):
                 x = colimn_index * TILESIZE
@@ -26,6 +27,26 @@ class Level:
                     self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self) -> None:
-        self.visible_sprites.draw(self.diplay_surface)
+        # self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self) -> None:
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_width() // 2
+        self.half_height = self.display_surface.get_height() // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player) -> None:
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_position)
